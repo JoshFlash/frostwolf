99035f202b58d8838ad3bdcf89101e3922b9cb20
Joshua Flash <joshuaflash@gmail.com>
2023-04-18 22:49:27 +1200

    initial project setup

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <boost/version.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    std::cout << "Hello, Frostwolf!" << std::endl;
+    std::cout << "Boost version: " << BOOST_LIB_VERSION << std::endl;
+
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Create a windowed mode window and its OpenGL context
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Frostwolf", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Load OpenGL function pointers using GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    return 0;
+}
